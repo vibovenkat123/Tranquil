399bb8ba5d9687560a3422fcd9a768c4270749de
vibovenkat123 <vaibhav@vaibhavvenkat.com>
2023-06-22 19:04:40 -0500

    Initial Commit

---
+++ Tranquil/TranquilApp.swift
@@ -0,0 +1,20 @@
+//
+//  TranquilApp.swift
+//  Tranquil
+//
+//  Created by Vaibhav Venkat on 6/22/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TranquilApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: Item.self)
+    }
+}
