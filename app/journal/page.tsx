e5d8bd3db7d6e3da4fce5ea1062bf3e773b8edfc
vibovenkat123 <vaibhav@vaibhavvenkat.com>
2023-07-02 21:12:32 -0500

    feat: remove home button

--- app/journal/page.tsx
+++ app/journal/page.tsx
@@ -9,59 +9,45 @@ import PreviewCard from "../components/Journal/PreviewCard";
 import { Home } from "lucide-react";
 
 export default function Journal(): React.ReactElement {
-  const [entries, setEntries] = useState<Entry[]>([]);
-  const [currentEntry, setCurrentEntry] = useState<Entry | null>(null);
-  useEffect(() => {
-    setEntries(getEntries());
-  }, []);
-  const cards: React.ReactElement[] = entries.map((entry) => {
+    const [entries, setEntries] = useState<Entry[]>([]);
+    const [currentEntry, setCurrentEntry] = useState<Entry | null>(null);
+    useEffect(() => {
+        setEntries(getEntries());
+        if (getEntries().length > 0) {
+            if (getEntries()[0].date.toDateString = new Date().toDateString) {
+                const entry = validateEntry(
+                    ADJECTIVE.NEUTRAL,
+                    "Talk about your day"
+                );
+                if (entry) {
+                    const newEntryAdded = newEntry(entry);
+                    setEntries(getEntries());
+                    setCurrentEntry(newEntryAdded);
+                }
+            }
+        }
+    }, []);
+    const cards: React.ReactElement[] = entries.map((entry) => {
+        return (
+            <PreviewCard
+                setEntries={setEntries}
+                key={entry.date.getTime()}
+                entry={entry}
+                setCurrentEntry={setCurrentEntry}
+            />
+        );
+    });
     return (
-      <PreviewCard
-        setEntries={setEntries}
-        key={entry.date.getTime()}
-        entry={entry}
-        setCurrentEntry={setCurrentEntry}
-      />
+        <main className="flex flex-col w-full">
+            <Header />
+            <div className="w-full flex h-auto">
+                <div className="w-1/4 md:w-1/5 border-r-2 border-border h-screen p-4">
+                    {cards}
+                </div>
+                <div className="w-4/5 h-screen p-4">
+                    <JournalEntry entry={currentEntry} setEntries={setEntries} />
+                </div>
+            </div>
+        </main>
     );
-  });
-  return (
-    <main className="flex flex-col w-full">
-      <Header />
-      <div className="w-full flex h-auto">
-        <div className="w-1/4 md:w-1/5 border-r-2 border-border h-screen p-4">
-          <Button
-            className="w-full mt-5 flex-col gap-2 p-8 border-border border-2"
-            variant="outline"
-            onClick={() => {
-              setCurrentEntry(null);
-            }}
-          >
-            <h2>
-              <Home />
-            </h2>
-          </Button>
-          {cards}
-        </div>
-        <div className="w-4/5 h-screen p-4">
-          <Button
-            className={`${currentEntry ? "hidden" : "block"}`}
-            onClick={() => {
-              const entry = validateEntry(
-                ADJECTIVE.NEUTRAL,
-                "Talk about your day"
-              );
-              if (entry) {
-                const newEntryAdded = newEntry(entry);
-                setEntries(getEntries());
-                setCurrentEntry(newEntryAdded);
-              }
-            }}
-          >
-            Add
-          </Button>
-          <JournalEntry entry={currentEntry} setEntries={setEntries} />
-        </div>
-      </div>
-    </main>
-  );
 }
